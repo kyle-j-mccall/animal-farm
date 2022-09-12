2b5706805c1e015be9f643964ed07454420ff160
Kyle McCall <kylem147@gmail.com>
2022-09-12 16:22:55 -0700

    renders background and animals with animal compenent

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,12 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+export default function Animal({ animal }) {
+  return <>
+    <div className='animal' style={{ left: animal.left, top: animal.top }}>
+      <p className='name'>{animal.name}</p>
+      <p>{animal.type}</p>
+      <p>{animal.says}</p>
+      <img src={`${process.env.PUBLIC_URL}/animals/${animal.type}.svg`}/>
+    </div>
+  </>;
 }
